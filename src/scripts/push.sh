ebb9b147252b4de458498f5e1a7662f1561129c9
Anup Chenthamarakshan <anupcshan@gmail.com>
2022-08-03 16:36:05 -0700

    Evaluate env vars in image name (#4)

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+# Evaluate env vars in image name
+PARAM_DOCKER_IMAGE=$(eval echo "${PARAM_DOCKER_IMAGE}")
+
 ADDL_FLAGS=""
 if [[ -n ${PARAM_WAIT_CHANNELS} ]]; then
 	ADDL_FLAGS="--wait-channel=\"${PARAM_WAIT_CHANNELS}\""
