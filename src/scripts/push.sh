df5ba4204091066c78bd037298ad3bf957f3024c
Mike Cugini <mike@prodvana.io>
2022-08-08 11:17:08 -0400

    fix(push): push with no wait-channels no longer sends an empty string (#9)
    Due to how the wait-channels optional arg was being quoted, pvnctl would
    recieve an extra (empty) argument

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 # Evaluate env vars in image name
 PARAM_DOCKER_IMAGE=$(eval echo "${PARAM_DOCKER_IMAGE}")
 
-ADDL_FLAGS=""
+CMD="pvnctl services update-images ${PARAM_SERVICE} --image ${PARAM_DOCKER_IMAGE}"
 if [[ -n ${PARAM_WAIT_CHANNELS} ]]; then
-	ADDL_FLAGS="--wait-channel=\"${PARAM_WAIT_CHANNELS}\""
+    CMD="$CMD --wait-channel=${PARAM_WAIT_CHANNELS}"
 fi
+
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
-pvnctl services update-images "${PARAM_SERVICE}" --image "${PARAM_DOCKER_IMAGE}" "${ADDL_FLAGS}"
+eval "${CMD}"
