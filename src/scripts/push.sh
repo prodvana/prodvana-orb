af8d749d7077ac808e7cc894ca26e43d38371119
Naphat Sanguansin <naphat.krit@gmail.com>
2022-08-13 00:12:54 +0000

    support application

--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # Evaluate env vars in image name
 PARAM_DOCKER_IMAGE=$(eval echo "${PARAM_DOCKER_IMAGE}")
 
-CMD="pvnctl services update-images ${PARAM_SERVICE} --image ${PARAM_DOCKER_IMAGE}"
+CMD="pvnctl services --app=${PARAM_APP} update-images ${PARAM_SERVICE} --image ${PARAM_DOCKER_IMAGE}"
 if [[ -n ${PARAM_WAIT_CHANNELS} ]]; then
     CMD="$CMD --wait-channel=${PARAM_WAIT_CHANNELS}"
 fi
