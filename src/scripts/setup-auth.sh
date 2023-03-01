4e15cf607cbc1b7ecf7451c8fd3f0668a6841499
Mike Cugini <mike@prodvana.io>
2023-03-01 15:52:37 +0000

    update default api port

--- src/scripts/setup-auth.sh
+++ src/scripts/setup-auth.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 PARAM_API_TOKEN=$(eval echo "\$$PARAM_API_TOKEN")
 
-pvnctl auth context add "${PARAM_CONTEXT}" "api.${PARAM_ORG}.prodvana.io:5000"
+pvnctl auth context add "${PARAM_CONTEXT}" "api.${PARAM_ORG}.prodvana.io:443"
 pvnctl auth token "${PARAM_API_TOKEN}"
