ec46eb453b03b729d08c924c7cf7c6a045c94625
Naphat Sanguansin <naphat.krit@gmail.com>
2023-03-15 19:28:12 +0000

    try

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -6,4 +6,4 @@ PARAM_PATH=$(eval echo "${PARAM_PATH}")
 
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
 # intentionally not quoting PARAM_PATH so that wildcards get evaluated.
-pvnctl configs "${PARAM_TYPE}" apply "${PARAM_PATH}"
+pvnctl configs "${PARAM_TYPE}" apply "${#PARAM_PATH[@]}"
