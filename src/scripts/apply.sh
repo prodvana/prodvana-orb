cd24ad2add507f5504cdb474449990d067a4def1
Naphat Sanguansin <naphat.krit@gmail.com>
2023-03-15 19:35:03 +0000

    done

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 # Evaluate globs
 PARAM_PATH=$(eval echo "${PARAM_PATH}")
+PARAM_ARRAY=(`echo ${PARAM_PATH} | tr ' ' ' '`)
 
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
 pvnctl configs "${PARAM_TYPE}" apply "${PARAM_PATH[@]}"
