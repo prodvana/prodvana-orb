ab6f84ff4c0ea5fda2861839d1bded55b8e6b296
Naphat Sanguansin <naphat.krit@gmail.com>
2023-03-15 19:10:33 +0000

    comment

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -2,4 +2,5 @@
 set -euxo pipefail
 
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
+# intentionally not quoting PARAM_PATH so that wildcards get evaluated.
 pvnctl configs "${PARAM_TYPE}" apply ${PARAM_PATH}
