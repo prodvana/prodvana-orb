574452e6ff4dde7e7be6a376e1233e15ed5c4cfb
Naphat Sanguansin <naphat.krit@gmail.com>
2023-03-15 19:33:57 +0000

    keep

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 
 # Evaluate globs
 PARAM_PATH=$(eval echo "${PARAM_PATH}")
-PARAM_ARRAY=(`echo ${PARAM_PATH} | tr ',' ' '`)
 
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
-pvnctl configs "${PARAM_TYPE}" apply "${PARAM_ARRAY[@]}"
+pvnctl configs "${PARAM_TYPE}" apply "${PARAM_PATH[@]}"
