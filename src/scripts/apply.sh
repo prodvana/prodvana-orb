356e8ae33365119096137fe1f5a81ba941b095a2
Naphat Sanguansin <naphat@prodvana.io>
2023-09-25 19:22:13 -0700

    fix up configs apply command (#15)

--- src/scripts/apply.sh
+++ src/scripts/apply.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Evaluate globs
-PARAM_PATH=$(eval echo "${PARAM_PATH}")
-PARAM_ARRAY=()
-read -ra PARAM_ARRAY <<<"${PARAM_PATH}"
-
 pvnctl auth context use "${PARAM_AUTH_CONTEXT}"
-pvnctl configs "${PARAM_TYPE}" apply "${PARAM_ARRAY[@]}"
+pvnctl configs apply "${PARAM_PATH}"
